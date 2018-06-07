0bbdc0d2a8790fcc532698f543e93173e2992677
Hamilton Gabriel <gabrielzinho.gabriel2@gmail.com>
2018-06-07 06:00:59 -0400

    Enumerando Matrizes

---
+++ src/components/utilizandovfor/CriandoListaDinamicaEOrdenada.vue
@@ -0,0 +1,21 @@
+<template>
+    <div>
+        <ul>
+            <li v-for="(n, key) in counteDown" :key="key">{{n}}</li>
+            <li>Lançar Míssil!</li>
+        </ul>
+    </div>
+</template>
+<script>
+export default {
+  data: () => ({
+    counteDown: []
+  }),
+  created() {
+    var counter = 10;
+    setInterval(() => {
+      counter > 0 ? this.counteDown.push(counter--) : "";
+    }, 1000);
+  }
+};
+</script>
--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,6 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import HelloWorld from '@/components/utilizandovfor/EnumeracaoDeArrays'
+import HelloWorld from '@/components/utilizandovfor/CriandoListaDinamicaEOrdenada'
 
 Vue.use(Router)
 
