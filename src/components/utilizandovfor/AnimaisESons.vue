ceccfdfe9f6877c9cc40cb72dd32b5ea20fccdff
Hamilton Gabriel <gabrielzinho.gabriel2@gmail.com>
2018-06-07 05:11:29 -0400

    Animais e Seus Sons

---
+++ src/components/utilizandovfor/AnimaisESons.vue
@@ -0,0 +1,30 @@
+<template>
+    <div>
+        <ul>
+            <li v-for="(animais, key) in animals" :key="key">
+                O {{animais.typeAnimal}} faz {{animais.sound}}
+            </li>
+        </ul>
+    </div>
+</template>
+<script>
+export default {
+  data: () => ({
+    animals: [
+      {
+        typeAnimal: "Gato",
+        sound: "Meow"
+      },
+      {
+        typeAnimal: "Passarinho",
+        sound: "Tweet"
+      },
+      {
+        typeAnimal: "Cachorro",
+        sound: "Au Au"
+      }
+    ]
+  })
+};
+</script>
+
--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,6 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import HelloWorld from '@/components/ContagemRegressiva'
+import HelloWorld from '@/components/utilizandovfor/AnimaisESons'
 
 Vue.use(Router)
 
