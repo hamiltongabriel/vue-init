484e3cd3e5fab38f3a23874c064c65036e7af614
Hamilton Gabriel <gabrielzinho.gabriel2@gmail.com>
2018-06-07 05:21:54 -0400

    Enumerando Matrizes

---
+++ src/components/utilizandointerpolacao/EnumerandoMatrizes.vue
@@ -0,0 +1,18 @@
+<template>
+    <div>
+        <ul>
+            <li v-for="(valor, index, key) in dados" :key="key">{{index}}{{valor}}</li>
+        </ul>
+    </div>
+</template>
+<script>
+export default {
+  data: () => ({
+      dados:{
+          firstName: 'Hamilton',
+          lastName: 'gabriel',
+          age: 19
+      }
+  })
+}
+</script>
