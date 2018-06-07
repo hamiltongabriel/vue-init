1cc8a8eb6c111d5abc79d01853b4494de85d05fa
Hamilton Gabriel <gabrielzinho.gabriel2@gmail.com>
2018-06-07 04:41:05 -0400

    Contagem Regressiva

--- src/components/ContagemRegressiva.vue
+++ src/components/ContagemRegressiva.vue
@@ -2,8 +2,15 @@
     <div>
         <ul>
             <!-- Anti padrao, nao devemos colocar matrizes literais dentro da marcacao -->
-            <li v-for="(n, key) in [10, 9, 8, 7,  6, 5, 4, 3, 2, 1,]" :key="key">{{ n }}</li>
+            <li v-for="(n, key) in countDown" :key="key">{{ n }}</li>
             <li>Lançar Míssil.</li>
         </ul>
     </div>
 </template>
+<script>
+    export default{
+        data:() => ({
+            countDown: [10, 9, 8, 7, 6, 5, 4, 3, 2, 1]
+        })
+    }
+</script>
