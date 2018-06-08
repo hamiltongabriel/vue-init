201350d2930276c5c1a209baf20891932ef8cd2b
Hamilton Gabriel <gabrielzinho.gabriel2@gmail.com>
2018-06-08 06:02:38 -0400

    Adicionando e Removendo dados em tempo Real.

--- src/components/utilizandovmodel/CidadesQuePretendoVisitar.vue
+++ src/components/utilizandovmodel/CidadesQuePretendoVisitar.vue
@@ -1,18 +1,27 @@
 <template>
     <div>
+        Cidades que pretendo Visitar.
         <ul>
             <li v-for="(city, key, i) in citys" :key="key">{{city}}
             <button @click="citys.splice(i, 1)">Remover Cidade</button></li>
         </ul>
         <input v-model="newCity"/>
-        <button @click="citys.push(this.newCity)">Adicionar nova cidade</button>
+        <button @click="addCity">Adicionar nova cidade</button>
     </div>
 </template>
 <script>
 export default {
   data: () => ({
-      citys: ['Vitoria', 'Bahia', 'Floripa'],
-      newCity: []
-  })
+    citys: ["Vitoria", "Bahia", "Floripa"],
+    newCity: []
+  }),
+  methods: {
+    removeCity() {
+      this.citys.splice(i, 1);
+    },
+    addCity() {
+      this.citys.push(this.newCity);
+    }
+  }
 };
 </script>
