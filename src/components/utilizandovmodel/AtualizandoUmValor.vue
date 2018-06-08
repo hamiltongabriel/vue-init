a242ad861bcba2cb65df6d2e89686b6a40b73c03
Hamilton Gabriel <gabrielzinho.gabriel2@gmail.com>
2018-06-08 05:27:22 -0400

    Atualizacao de Dados em tempo real

---
+++ src/components/utilizandovmodel/AtualizandoUmValor.vue
@@ -0,0 +1,20 @@
+<template>
+<div>
+    <button @click="orderFood">Fazer pedido</button>
+    <input v-model="foodRequested" />Quantidades de comidas solicitadas até o momento: {{quantityFoodFinal}}
+</div>
+</template>
+<script>
+export default {
+//Usamos v-model para manter uma atualizacao real (fazendo uma vinculacao de dados bidirecional em 2 sentidos.)
+  data: () => ({
+      foodRequested: 0,
+      quantityFoodFinal: null
+  }),
+  methods:{
+    orderFood(){
+        this.quantityFoodFinal = `${this.foodRequested}` + `${this.quantityFoodFinal}`;
+    }
+  }
+};
+</script>
--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,6 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import HelloWorld from '@/components/utilizandovon/ReagindoAosClicks'
+import HelloWorld from '@/components/utilizandovmodel/AtualizandoUmValor'
 
 Vue.use(Router)
 
