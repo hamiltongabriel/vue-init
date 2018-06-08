8bb76e7a4c9f5e6733cb60aa81123d7e3271c69a
Hamilton Gabriel <gabrielzinho.gabriel2@gmail.com>
2018-06-08 05:03:03 -0400

    Reagindo aos Clicks

---
+++ src/components/utilizandovon/ReagindoAosClicks.vue
@@ -0,0 +1,14 @@
+<template>
+    <div>
+        <button @click="orderFood">Fazer pedido.</button>
+    </div>
+</template>
+<script>
+    export default{
+        methods:{
+            orderFood(){
+                alert('Seu pedido esta na mesa')
+            }
+        }
+    }
+</script>
--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,6 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import HelloWorld from '@/components/utilizandovfor/CriandoListaDinamicaEOrdenada'
+import HelloWorld from '@/components/utilizandovon/ReagindoAosClicks'
 
 Vue.use(Router)
 
