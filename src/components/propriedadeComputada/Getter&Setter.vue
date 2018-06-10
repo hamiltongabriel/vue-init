f22aab12a4fde199f7b643617439716b8118d4e2
Hamilton Gabriel <gabrielzinho.gabriel2@gmail.com>
2018-06-10 04:31:15 -0400

    Propriedade Computada

---
+++ src/components/propriedadeComputada/Getter&Setter.vue
@@ -0,0 +1,25 @@
+<template>
+    <div>
+        <p>Nome Computado: {{nomeFormatado}}</p>
+        <label>Nome a ser computado:</label>
+        <input type="text" v-model="nomeFormatado">
+    </div>
+</template>
+<script>
+export default {
+  data: () => ({
+    nome: "Hamilton Gabrikel"
+  }),
+  computed: {
+    nomeFormatado: {
+      get() {
+        console.log("Usando Computed");
+        return this.nome.toUpperCase();
+      },
+      set(novoValor) {
+        return (this.nome = novoValor.substring(0, 3));
+      }
+    }
+  }
+};
+</script>
--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,6 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import HelloWorld from '@/components/propriedadeComputada/NomeFormatado'
+import HelloWorld from '@/components/propriedadeComputada/Getter&Setter'
 
 Vue.use(Router)
 
