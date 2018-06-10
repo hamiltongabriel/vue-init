aed9dadebf8efc435354a70e77e1b5722f58ba50
Hamilton Gabriel <gabrielzinho.gabriel2@gmail.com>
2018-06-10 03:41:45 -0400

    Propriedade Computada

---
+++ src/components/propriedadeComputada/ResultadoRetornadoDaFuncao.vue
@@ -0,0 +1,21 @@
+<template>
+    <div>
+        a = {{ a }}, b = {{ b }}
+        <input v-model="a"/>
+        <pre>
+            {{$data | json}}
+        </pre>
+    </div>
+</template>s
+<script>
+export default {
+  data: () => ({
+    a: 1
+  }),
+  computed: {
+    b() {
+      return parseInt(this.a) + parseInt(1);
+    }
+  }
+};
+</script>
