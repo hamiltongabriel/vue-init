4e535b2a326f554a22e9d91aad5db8e088b021ce
Hamilton Gabriel <gabrielzinho.gabriel2@gmail.com>
2018-06-09 21:41:23 -0400

    Propriedade Computada

---
+++ src/components/propriedadeComputada/ArmazenandoEmCache.vue
@@ -0,0 +1,18 @@
+<template>
+<div>
+{{doubleValue}}
+</div>
+</template>
+<script>
+    export default{
+        data:() => ({
+            value: 10
+        }),
+        computed:{
+            doubleValue(){
+                console.log('doubleValue computed property changed');
+                return this.value * 2
+            }
+        }
+    }
+</script>
---
+++ src/components/propriedadeComputada/ComparacaoDeValor.vue
@@ -0,0 +1,26 @@
+<template>
+    <div>
+        <p>
+            Soma dos Numeros: {{ numberTotal }}
+        </p>
+    </div>
+</template>
+<script>
+export default {
+  data: () => ({
+    numbers: [5, 8, 3]
+  }),
+  computed: {
+    numberTotal: {
+      get() {
+        return this.numbers.reduce((sum, val) => sum + val);
+      },
+      set(newValue){
+          const oldValue = this.numberTotal;
+          const difference = newValue - oldValue;
+          this.numbers.push(difference)
+      }
+    }
+  }
+};
+</script>
