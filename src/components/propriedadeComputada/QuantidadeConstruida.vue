541212e9ad4abd85dbe7d40ca0c522e327276bf9
Hamilton Gabriel <gabrielzinho.gabriel2@gmail.com>
2018-06-16 21:56:57 -0400

    Propriedade Computada

--- src/components/propriedadeComputada/QuantidadeConstruida.vue
+++ src/components/propriedadeComputada/QuantidadeConstruida.vue
@@ -18,6 +18,7 @@ export default {
     legCount: 0
   }),
   computed: {
+
     tableCount: {
       // Descobrindo o numero de tabelas.
       // pega o valor da propriedade
@@ -25,6 +26,7 @@ export default {
         return this.legCount / 4
       },
       // Define o numero de Tabelas
+
       set (newValue) {
         this.legCount = newValue * 4
       }
