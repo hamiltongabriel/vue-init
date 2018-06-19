b0d176691bf47c3b6e1c050f3d882c7c761e4911
Hamilton Gabriel <gabrielzinho.gabriel2@gmail.com>
2018-06-18 22:39:34 -0400

    Utilizando fudnamentos de estilo, com propriedades computadas, sinalizando quando chegar no limite.

---
+++ src/components/adicionandoEstilosCondicionalmente/FundamentosDeEstilo.vue
@@ -0,0 +1,36 @@
+<template>
+  <div>
+    <textarea
+      v-model="memeText"
+      :class="{ warn: longText }"
+      :maxlength="limit">
+    </textarea>
+    {{memeText.length}}
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'FundamentosDeEstilo',
+  data: () => ({
+    memeText: 'What if I told you ' +
+    'CSS can do that',
+    limit: 50
+  }),
+  computed: {
+    longText () {
+      if (this.limit - this.memeText.length <= 10){
+        return true
+      } else {
+        return false;
+      }
+    }
+  }
+}
+</script>
+
+<style scoped>
+  .warn {
+    background-color: mistyrose
+  }
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,6 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import HelloWorld from '@/components/exibindoEOcultandoElemento/UtilizandoVShow'
+import HelloWorld from '@/components/adicionandoEstilosCondicionalmente/FundamentosDeEstilo'
 
 Vue.use(Router)
 
