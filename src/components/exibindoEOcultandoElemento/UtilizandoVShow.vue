cbaaae601b9626adc880c85db1ade98977225f76
Hamilton Gabriel <gabrielzinho.gabriel2@gmail.com>
2018-06-18 22:18:51 -0400

    Utilizando v-show

---
+++ src/components/exibindoEOcultandoElemento/UtilizandoVShow.vue
@@ -0,0 +1,23 @@
+<template>
+   <div>
+     <div v-show="isNight">
+       Ta me vendo!
+     </div>
+   </div>
+</template>
+
+<script>
+export default {
+  name: 'UtilizandoVShow',
+  computed: {
+    // aparecera so antes das 7.
+    isNight () {
+      return new Date().getHours() < 7
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,6 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import HelloWorld from '@/components/formatandoDatasComFiltros/FormatacaoDeData'
+import HelloWorld from '@/components/exibindoEOcultandoElemento/UtilizandoVShow'
 
 Vue.use(Router)
 
