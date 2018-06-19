0017f1cd664150cb62b4ab5f19718e494323e59d
Hamilton Gabriel <gabrielzinho.gabriel2@gmail.com>
2018-06-19 19:26:55 -0400

    Usando transition e incrementando com v-show.

---
+++ src/components/efeitosAplicadosNoDOM/AlteracaoAplicadasAoDom.vue
@@ -0,0 +1,34 @@
+<template>
+   <div>
+   <!-- trannsitions são efeitos que podem ser aplicados quando elementos são inseridos,
+atualizado e removido do DOM. -->
+     <article>
+       Nao sou fruta<br>
+       Nao sou peixe.<br>
+       Nao sou cachorro.<br>
+       Nao sou nenhum desses.
+       <div id="solution" @click="showSolution = true"> eu sou um <transition name="fade"><span id="dragon" v-show="showSolution">dragon</span> </transition></div>
+     </article>
+   </div>
+</template>
+
+<script>
+export default {
+  name: 'AlteracaoAplicadasAoDom',
+  data: () => ({
+    showSolution: false
+  })
+}
+</script>
+
+<style scoped>
+#solution {
+  cursor: pointer;
+}
+.fade-enter {
+  opacity: 0
+}
+.fade-enter-active {
+  transition: opacity .5s;
+}
+</style>
