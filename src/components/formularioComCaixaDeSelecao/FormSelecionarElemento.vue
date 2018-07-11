9a27032a0599bbc4a2969975c0b5b3908ed4f376
Hamilton Gabriel <gabrielzinho.gabriel2@gmail.com>
2018-07-10 22:50:43 -0400

    Animais fazenda.

--- src/components/formularioComCaixaDeSelecao/FormSelecionarElemento.vue
+++ src/components/formularioComCaixaDeSelecao/FormSelecionarElemento.vue
@@ -3,13 +3,17 @@
     {{choosenCountry}}
     <fieldset>
       <select v-model="clan">
+        <!-- Create menu suspense-->
         <option v-for="(types, clan) in clans">{{ clan }}</option>
       </select>
+      <!--Preenchendo o tipo de uma determinada clan-->
       <select v-model="type">
         <option v-for="(species, type) in clans[clan]">{{type}}</option>
       </select>
+      <!--Se a variavel lan tiver valor, o select permite escolher o tipo animal-->
+      <!--Como os clans sao indefinidos ira emitir um erro, usamos um v-if para solucionar o problema-->
       <select>
-        <option v-for="(animals, species) in clans[clan]">{{species}}</option>
+        <option v-for="(animals, species) in clans[clan][type]">{{species}}</option>
       </select>
     </fieldset>
   </div>
