08dfc0ac3ffc89cf7cf406fdea914d103ae917d8
Hamilton Gabriel <gabrielzinho.gabriel2@gmail.com>
2018-08-07 04:44:38 -0400

    Fazendo o uso de Getters And Setters Quando necessario

---
+++ src/ch2/propertyComputed/computedSetters.vue
@@ -0,0 +1,36 @@
+<template>
+    <div>
+        <label>Legs: <input v-model="legCount" type="range"></label>
+        <label>Tops: <input @input="update" :value="tableCount"></label>
+        <output>
+            We are going to build {{legCount}} Legs
+            and assembly {{tableCount}} tables.
+        </output>
+    </div>
+</template>
+<script>
+// Quando for mais simples fazer uma alteracao direta do que modificar as dependencias reativas da pagina
+// Use um setter.
+export default {
+    data: () => ({
+        legCount: 0
+    }),
+    // Criando o numero de tabelas.
+    computed: {
+        tableCount: {
+            get(){
+                return this.legCount / 4
+            },
+            set (newValue) {
+                this.legCount = newValue * 4
+            }
+        }
+    },
+    methods: {
+              update(e) {
+                debugger
+             this.tableCount = e.target.value
+        }
+    }
+}
+</script>
--- src/router/index.js
+++ src/router/index.js
@@ -1,7 +1,7 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 // import HelloWorld from '@/components/TransitionsAndAnimations/Taxi'
-import HelloWorld from '@/ch2/propertyComputed/nameSurname'
+import HelloWorld from '@/ch2/propertyComputed/computedSetters'
 Vue.use(Router)
 
 export default new Router({
