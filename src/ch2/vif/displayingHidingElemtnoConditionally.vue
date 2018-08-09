437c84a91acae05e4282059c93334bcb70dfefd3
Hamilton Gabriel <gabrielzinho.gabriel2@gmail.com>
2018-08-08 22:43:33 -0400

    V-if v-show

---
+++ src/ch2/vif/displayingHidingElemtnoConditionally.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <div v-show="isNight">
+      I'm a ghost! Boo!
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data: () => ({
+    isNight: true
+  }),
+  computed: {
+    isNight () {
+      return new Date().getHours() < 7
+    }
+  }
+}
+</script>
--- src/router/index.js
+++ src/router/index.js
@@ -1,7 +1,7 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 // import HelloWorld from '@/components/TransitionsAndAnimations/Taxi'
-import HelloWorld from '@/ch2/propertyComputed/sortingListWithComputedProperty'
+import HelloWorld from '@/ch2/vif/displayingHidingElemtnoConditionally'
 Vue.use(Router)
 
 export default new Router({
